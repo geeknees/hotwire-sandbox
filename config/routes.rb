498c50606b293543a717ea1d20b0f8f985b962f9
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2020-12-31 13:14:38 +0900

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :tickets do
     resources :comments, only: [:new, :create]
   end
+  root to: 'tickets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
