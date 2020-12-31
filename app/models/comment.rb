94c6457a771743ad799615a0d61a14bbae2f4e52
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2020-12-31 13:43:37 +0900

    Implement hotwire

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   has_rich_text :content
   belongs_to :ticket
+  broadcasts_to :ticket
+  # after_create_commit -> { broadcast_append_to ticket }
+  # after_destroy_commit -> { broadcast_remove_to ticket }
+  # after_update_commit -> { broadcast_replace_to ticket }
 end
